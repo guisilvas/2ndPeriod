ca0ca0df60e82e2687e11961e7c84f091ebc1917
guisilvas <guilherme7soaress@gmail.com>
2024-08-10 17:37:46 -0300

    Update clike, add ifelse codes

--- aedsII/clike/operators/operators.cpp
+++ aedsII/clike/operators/operators.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 int main()
 {
-    int x=2, y=3;
-    int sum, sub, mult, remainder, quoc;
+    int x=3, y=2;
+    int sum, sub, mult, remainder, quoc, inc, dec;
     float div;
 
     sum = x+y;
@@ -14,12 +14,16 @@ int main()
     cout << "x - y = " << sub << "\n";
     mult = x*y;
     cout << "x * y = " << mult << "\n";
-    div = x/y;
+    div = (float)x/y;
     cout << "x / y = " << div << "\n";
     remainder = x%y;
     cout << "x % y = " << remainder << "\n";
     quoc = x/y;
     cout << "x / y = " << quoc << "\n";
+    inc = x+1;
+    cout << x << "++ = " << inc << "\n";
+    dec = y-1;
+    cout << y << "-- = " << dec << "\n";
 
     return 0;
 }
